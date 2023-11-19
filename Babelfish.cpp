321f8327802732fa3efcbb882a2c89b6094589ef
G31415 <1198055805@qq.com>
2023-11-19 21:51:56 +0800

    git commit

--- Babelfish.cpp
+++ Babelfish.cpp
@@ -1,7 +1,43 @@
 // poj2503 Babelfish
-#include<iostream>
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 using namespace std;
 
+#define MAX 1000
+#define max 100
+
+char a[MAX][max];
+char b[MAX][max];
+
+int main()
+{
+    int i = 0;
+    char temp[max * 2 + 3];
+    while (gets(temp) && (int)strlen(temp) != 0)
+    {
+
+        sscanf(temp, "%s %s", a[i], b[i]); // 输入格式
+        printf("%s %s", a[i], b[i]);
+        i++;
+    }
+
+    return 0;
+}
+
+// char c[MAX][max];
+
+// {
+
+//     x[c] = b;
+// }
+// while (gets(a))
+// {
+//     if (x.find(a) != x.end())
+//         cout << x[a] << endl;
+//     else
+//         cout << "eh" << endl;
+// }
+// return 0;
--- text.cpp
+++ text.cpp
@@ -1,3 +1,28 @@
-
-
-
+#include <cstdio>
+#include <map>
+#include <string.h>
+#include <iostream>
+using namespace std;
+map<string, string> x;
+int main(void)
+{
+    char a[100], b[50], c[50];
+    while (gets(a) && strlen(a) != 0)
+    {
+        int i, j, k;
+        for (i = 0; a[i] != ' '; i++)
+            b[i] = a[i];
+        b[i] = '\0';
+        for (j = i + 1, k = 0; j <= strlen(a); k++, j++)
+            c[k] = a[j];
+        x[c] = b;
+    }
+    while (gets(a))
+    {
+        if (x.find(a) != x.end())
+            cout << x[a] << endl;
+        else
+            cout << "eh" << endl;
+    }
+    return 0;
+}
