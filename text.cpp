353117b308d4e372be45e53f34d2e1e4805facdb
G31415 <1198055805@qq.com>
2023-11-20 23:18:16 +0800

    git commit

--- text.cpp
+++ text.cpp
@@ -1,58 +1,91 @@
-#include <iostream>
+#include <algorithm>
 #include <cmath>
-#include <iomanip>
+#include <cstdio>
+#include <cstring>
+#include <vector>
 using namespace std;
 
-struct Node
+#define inf 0x3f3f3f3f
+#define maxn 100000
+
+struct p
 {
-    double x;
-    double y;
+    int x, y, id;
+    bool operator<(const p &a) const
+    {
+        if (x != a.x)
+            return x < a.x;
+        return y < a.y;
+    }
 };
 
-float Gzz(Node *stations, Node *agents, int k)
+double dist(const p &a, const p &b)
 {
-    double min = sqrt(pow(stations[0].x - agents[0].x, 2) + pow(stations[0].y - agents[0].y, 2));
-    for (int i = 0; i < k; i++)
+    double dx = a.x - b.x, dy = a.y - b.y;
+    return sqrt(dx * dx + dy * dy);
+}
+
+double solve(int l, int r, p ps[])
+{
+    if (r - l <= 1)
+        return inf;
+    if (r - l <= 3)
     {
-        for (int j = 0; j < k; j++)
+        double minDist = inf;
+        for (int i = l; i < r; i++)
         {
-            if (abs(stations[i].x - agents[j].x) > min || abs(stations[i].y - agents[j].y) > min)
-                continue;
-            else
+            for (int j = i + 1; j < r; j++)
             {
-                double distance = sqrt(pow(stations[i].x - agents[j].x, 2) + pow(stations[i].y - agents[j].y, 2));
-                if (min > distance)
-                    min = distance;
+                if (ps[i].id != ps[j].id)
+                    minDist = min(minDist, dist(ps[i], ps[j]));
             }
         }
+        return minDist;
     }
-    return min;
+
+    int m = (l + r) >> 1, limitx = ps[m - 1].x;
+    double res = min(solve(l, m, ps), solve(m, r, ps));
+
+    // 构建 strip 数组
+    vector<p> strip;
+    for (int i = l; i < r; i++)
+    {
+        if (abs(ps[i].x - limitx) < res)
+            strip.push_back(ps[i]);
+    }
+    sort(strip.begin(), strip.end(), [](const p &a, const p &b)
+         { return a.y < b.y; });
+
+    // 在 strip 数组中寻找最小距离
+    for (int i = 0; i < strip.size(); i++)
+    {
+        for (int j = i + 1; j < strip.size() && strip[j].y - strip[i].y < res; j++)
+        {
+            if (strip[i].id != strip[j].id)
+                res = min(res, dist(strip[i], strip[j]));
+        }
+    }
+
+    return res;
 }
 
 int main()
 {
-    int i;
-    cin >> i;
-    for (int i0 = 0; i0 < i; i0++)
+    int t, n, size;
+    scanf("%d", &t);
+    while (t--)
     {
-        int k;
-        cin >> k;
-        Node *stations = new Node[k];
-        Node *agents = new Node[k];
-        for (int n = 0; n < k; n++)
+        scanf("%d", &n);
+        size = n << 1;
+        p *ps = new p[size]; // dynamic allocation
+        for (int k = 0; k < size; k++)
         {
-            cin >> stations[n].x >> stations[n].y;
+            scanf("%d%d", &ps[k].x, &ps[k].y);
+            ps[k].id = k < n ? 0 : 1;
         }
-        for (int n = 0; n < k; n++)
-        {
-            cin >> agents[n].x >> agents[n].y;
-        }
-        double min = Gzz(stations, agents, k);
-        cout << fixed << setprecision(3) << min << endl;
-
-        // Remember to free the allocated memory
-        delete[] stations;
-        delete[] agents;
+        sort(ps, ps + size);
+        printf("%.3f\n", solve(0, size, ps));
+        delete[] ps; // deallocate the memory
     }
     return 0;
 }
