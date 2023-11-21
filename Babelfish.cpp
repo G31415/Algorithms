710346e4f9b582ff355853c98c32371d6d616ad8
G31415 <1198055805@qq.com>
2023-11-21 12:52:10 +0800

    git commit

--- Babelfish.cpp
+++ Babelfish.cpp
@@ -1,31 +1,4 @@
 // poj2503 Babelfish
-// #include <iostream>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <time.h>
-// using namespace std;
-
-// #define MAX 1000
-// #define max 100
-
-// char a[MAX][max];
-// char b[MAX][max];
-
-// int main()
-// {
-//     int i = 0;
-//     char temp[max * 2 + 3];
-//     while (gets(temp) && (int)strlen(temp) != 0)
-//     {
-//         sscanf(temp, "%s %s", a[i], b[i]); // 输入格式
-//         printf("%s %s", a[i], b[i]);
-//         i++;
-//     }
-//     return 0;
-// }
-
-/* -------------------------------------------------------------------------- */
 // 3264K  218ms
 #include <iostream>
 #include <cstdio>
