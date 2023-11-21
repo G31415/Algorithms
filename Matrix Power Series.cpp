b0e4a32e2514040740662fe02209df8817c29d6c
G31415 <1198055805@qq.com>
2023-11-21 13:12:35 +0800

    git commit

--- Matrix Power Series.cpp
+++ Matrix Power Series.cpp
@@ -1,1 +1,33 @@
 // poj3233 Matrix Power Series
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+#define Max 30
+int a[Max][Max];
+int n, k, m;
+
+int calculator(int **a, int **b)
+{
+}
+
+int solve()
+{
+}
+int main()
+{
+
+    scanf("%d %d %d", &n, &k, &m);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("%d", solve());
+    return 0;
+}
