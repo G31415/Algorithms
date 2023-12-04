f3daeec8d1893227db8f4d70b34bc5784ca568c2
G31415 <1198055805@qq.com>
2023-12-04 12:10:17 +0800

    git commit

--- 11_Median.cpp
+++ 11_Median.cpp
@@ -1,5 +1,49 @@
 // poj3579 Median
 
+// 288K	329MS
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+const int maxn = 100005, INF = 0x3f3f3f3f;
+int n, a[maxn];
+bool check(int x)
+{
+    int cnt = 0;
+    for (int i = 1, j = 1; i <= n; ++i)
+    {
+        while (a[j] <= a[i] + x && j <= n)
+            ++j;
+        --j;
+        cnt += j - i;
+    }
+    if (!(((n * (n - 1)) >> 1) & 1))
+        return cnt < (n * (n - 1)) >> 2;
+    else
+        return cnt <= (n * (n - 1)) >> 2;
+}
+int main()
+{
+    while (scanf("%d", &n) != EOF)
+    {
+        for (int i = 1; i <= n; ++i)
+            scanf("%d", &a[i]);
+        sort(a + 1, a + 1 + n);
+        int l = 0, r = INF;
+        while (l < r)
+        {
+            int mid = l + r >> 1;
+            if (check(mid))
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        printf("%d\n", l);
+    }
+    return 0;
+}
+
 // 416K	407MS
 // 给出N个数，对于存有每两个数的差值的序列求中位数，如果这个序列有偶数个元素，就取中间偏小的作为中位数。
 
