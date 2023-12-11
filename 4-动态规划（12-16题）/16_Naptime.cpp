2b9d24011eb916b34a756b5d27105bfeed4e6e4a
G31415 <1198055805@qq.com>
2023-12-11 21:44:38 +0800

    git commit

--- 4-动态规划（12-16题）/16_Naptime.cpp
+++ 4-动态规划（12-16题）/16_Naptime.cpp
@@ -1,2 +1,34 @@
 // poj2228 Naptime
+// 这是环形dp的一种，策略是执行两次dp，第一次在任意位置断链成环，按照线性dp求解，第二次则通过赋值和限制条件，算出把断开的位置连上的状态的解，然后比较两次dp的结果
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+int f1[4000][2], f2[4000][2]; // 每一个状态其实只跟上一个状态有关,所以第一维i =2~n是可以省略的,否则会mle
+int main()
+{
+    int n, b, i, j, k, u[4000];
+    cin >> n >> b;
+    for (i = 1; i <= n; i++)
+        cin >> u[i];
+    for (j = 0; j <= b; j++)
+        f1[j][0] = f1[j][1] = f2[j][0] = f2[j][1] = 1 << 31; // 全部初始化为负无穷
+    f1[0][0] = 0, f1[1][1] = 0;
+    f2[1][1] = u[1]; // f2[0][0]=0;这个加不加对结果没影响,我的理解是这个状态在f1里面已经算过了,所以f2没必要去考虑
+    for (i = 2; i <= n; i++)
+        for (j = b; j >= 0; j--)
+        { // 倒序
+            f1[j][0] = max(f1[j][0], f1[j][1]);
+            f2[j][0] = max(f2[j][0], f2[j][1]);
+            if (j)
+            {
+                f1[j][1] = max(f1[j - 1][0], f1[j - 1][1] + u[i]);
+                f2[j][1] = max(f2[j - 1][0], f2[j - 1][1] + u[i]);
+            }
+        }
+    int ans = 0;
+    ans = max(max(f1[b][0], f1[b][1]), f2[b][1]);
+    cout << ans << endl;
 
+    return 0;
+}
--- 5-贪心策略（17-20题）/19_Allowance.cpp
+++ 5-贪心策略（17-20题）/19_Allowance.cpp
@@ -1,1 +1,113 @@
 // poj3040 Allowance
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <limits.h>
+#include <algorithm>
+#include <cstring>
+#include <math.h>
+#include <set>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct Coin
+{
+    int value;
+    int num;
+    Coin() {}
+    Coin(int x, int y) : value(x), num(y) {}
+    bool operator<(const Coin &other) const//小于运算符重载，用于排序
+    {
+        return value < other.value;
+    }
+} coin[20];
+
+int used[20];
+int n;
+int c;
+
+int main()
+{
+    while (scanf("%d%d", &n, &c) != EOF)
+    {
+        for (int i = 0; i < n; i++)
+            scanf("%d%d", &coin[i].value, &coin[i].num);
+
+        int res = 0;
+        sort(coin, coin + n);
+        
+        for (int i = n - 1; i >= 0; i++)//从大到小进行选择
+        {
+            if (coin[i].value >= c)
+            {
+                res += coin[i].num;
+                coin[i].num = 0;
+            }
+            else
+                break;//小于则跳出循环
+        }
+        while (true)
+        {
+            memset(used, 0, sizeof(used));
+            int cou = c;
+
+            // 从大到小 先挑面值大的，总值不能超过c
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (coin[i].num != 0)
+                {
+                    int temp = cou / coin[i].value;
+                    temp = min(temp, coin[i].num);
+                    used[i] = temp;
+                    cou -= temp * coin[i].value;
+                    if (cou == 0)
+                        break;
+                }
+            }
+
+            // 从小到大 补足剩余的钱 总值可以超过c
+            if (cou > 0)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (coin[i].num > 0)
+                    {
+                        while (used[i] < coin[i].num)
+                        {
+                            cou -= coin[i].value;
+                            used[i]++;
+                            if (cou <= 0)
+                                break;
+                        }
+                        if (cou <= 0)
+                            break;
+                    }
+                }
+            }
+
+            if (cou > 0)
+                break;
+
+            int temp = INT_MAX;
+            for (int i = 0; i < n; i++)
+            {
+                if (used[i] != 0)
+                {
+                    temp = min(temp, coin[i].num / used[i]);
+                }
+            }
+
+            res += temp;
+            for (int i = 0; i < n; i++)
+            {
+                if (used[i] != 0)
+                    coin[i].num -= used[i] * temp;
+            }
+        }
+        printf("%d\n", res);
+    }
+    return 0;
+}
